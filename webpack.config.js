edce47ca06d785a99525c91b40a7c41ada6a8a1c
Michael Hegner <hegner123@gmail.com>
2021-07-10 07:05:21 -0400

    added production mode to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-module.exports = {
+(module.exports = {
   mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
@@ -44,4 +44,49 @@ module.exports = {
   optimization: {
     minimize: true,
   },
-};
+}),
+  {
+    mode: "production",
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+      hot: true,
+      compress: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+      ],
+    },
+
+    externals: {
+      config: JSON.stringify({
+        apiUrl: "/api",
+      }),
+    },
+    optimization: {
+      minimize: true,
+    },
+  };
